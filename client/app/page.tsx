bd0b07737d6cc190e53dea3586e7dedfdc8cb38c
Deni <cube327@tuta.io>
2023-07-04 00:25:20 +0200

    Update home page and add footer

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -3,8 +3,11 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div className="px-3">
-      <h1 className="mb-5 text-center">Contest Results</h1>
-      <p className="fs-5">This is a place for posting results from Rubik&apos;s cube meetups.</p>
+      <h1 className="mb-5 text-center">Cubing Contests</h1>
+      <p className="fs-5">
+        This is a place for hosting unofficial Rubik&apos;s cube competitions or meet-ups. All official WCA events are
+        supported, and new ones can be created too, including team events. Records are also tracked for all events.
+      </p>
       <Link href="/competitions" className="fs-5">
         See all contests
       </Link>
