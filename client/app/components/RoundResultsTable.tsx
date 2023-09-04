ea817a526e0656114283e984e8158ddec7755f77
Deni <cube327@tuta.io>
2023-09-04 10:45:33 +0200

    Improve the results output for team events

--- client/app/components/RoundResultsTable.tsx
+++ client/app/components/RoundResultsTable.tsx
@@ -6,8 +6,6 @@ import { IResult, IRound, IPerson, IEvent, IRecordType } from '@sh/interfaces';
 import { RoundProceed, RoundType } from '@sh/enums';
 import { getRoundCanHaveAverage, getRoundRanksWithAverage } from '@sh/sharedFunctions';
 
-const PERSONS_DELIMITER = <span className="ms-1 me-2">x</span>;
-
 const RoundResultsTable = ({
   round,
   event,
@@ -26,30 +24,6 @@ const RoundResultsTable = ({
 }) => {
   const roundCanHaveAverage = getRoundCanHaveAverage(round.format, event);
   const roundRanksWithAverage = getRoundRanksWithAverage(round.format, event);
-
-  const getNamesAndFlags = (personIds: number[]) => {
-    if (!persons) throw new Error('Name not found');
-
-    return (
-      <span className="d-flex">
-        {personIds.map((id, i) => {
-          const person = persons.find((p: IPerson) => p.personId === id);
-
-          if (person) {
-            return (
-              <span key={person.personId} className="d-flex align-items-center">
-                {i !== 0 && PERSONS_DELIMITER}
-                <PersonName person={person} />
-                <Country countryIso2={person.countryIso2} noText />
-              </span>
-            );
-          }
-
-          return <span key={person.personId}>{i !== 0 && PERSONS_DELIMITER}(name not found)</span>;
-        })}
-      </span>
-    );
-  };
 
   // Gets green highlight styling if the result is not DNF/DNS and made podium or is good enough to proceed to the next round
   const getRankingHighlight = (result: IResult) => {
@@ -86,39 +60,56 @@ const RoundResultsTable = ({
           </tr>
         </thead>
         <tbody>
-          {round.results.map((result: IResult) => (
-            <tr key={result.personIds.join()}>
-              <td className="ps-2" style={getRankingHighlight(result)}>
-                {result.ranking}
-              </td>
-              <td>{getNamesAndFlags(result.personIds)}</td>
-              <td>
-                <Time result={result} event={event} recordTypes={recordTypes} />
-              </td>
-              {roundCanHaveAverage && (
-                <td>
-                  <Time result={result} event={event} recordTypes={recordTypes} average />
-                </td>
-              )}
-              <td>
-                <Solves event={event} attempts={result.attempts} />
-              </td>
-              {onEditResult && (
-                <td className="py-1">
-                  <button type="button" onClick={() => onEditResult(result)} className="me-2 btn btn-primary btn-sm">
-                    Edit
-                  </button>
-                  <button
-                    type="button"
-                    onClick={() => onDeleteResult((result as any)._id)}
-                    className="btn btn-danger btn-sm"
-                  >
-                    Delete
-                  </button>
-                </td>
-              )}
-            </tr>
-          ))}
+          {round.results.map((result: IResult) =>
+            result.personIds.map((personId, i) => {
+              const person = persons.find((p: IPerson) => p.personId === personId);
+
+              return (
+                <tr key={personId}>
+                  <td className="ps-2" style={getRankingHighlight(result)}>
+                    {i === 0 ? result.ranking : ''}
+                  </td>
+                  <td>
+                    {person ? (
+                      <span key={person.personId} className="d-flex align-items-center">
+                        <PersonName person={person} />
+                        <Country countryIso2={person.countryIso2} noText />
+                      </span>
+                    ) : (
+                      <span key={person.personId}>(name not found)</span>
+                    )}
+                  </td>
+                  <td>{i === 0 && <Time result={result} event={event} recordTypes={recordTypes} />}</td>
+                  {roundCanHaveAverage && (
+                    <td>{i === 0 && <Time result={result} event={event} recordTypes={recordTypes} average />}</td>
+                  )}
+                  <td>{i === 0 && <Solves event={event} attempts={result.attempts} />}</td>
+                  {onEditResult && (
+                    <td className="py-1">
+                      {i === 0 && (
+                        <>
+                          <button
+                            type="button"
+                            onClick={() => onEditResult(result)}
+                            className="me-2 btn btn-primary btn-sm"
+                          >
+                            Edit
+                          </button>
+                          <button
+                            type="button"
+                            onClick={() => onDeleteResult((result as any)._id)}
+                            className="btn btn-danger btn-sm"
+                          >
+                            Delete
+                          </button>
+                        </>
+                      )}
+                    </td>
+                  )}
+                </tr>
+              );
+            }),
+          )}
         </tbody>
       </table>
     </div>
