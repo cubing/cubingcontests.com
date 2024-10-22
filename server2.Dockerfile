1a8b096ec2f73a88efd743dbbb59955641e80a16
Deni <cube327@tuta.io>
2024-10-22 23:15:28 +0200

    Sort out environment variables. Update docker configs. Fix static assets fetching.

--- server2.Dockerfile
+++ server2.Dockerfile
@@ -1,7 +1,12 @@
 FROM denoland/deno:distroless-2.0.2
 
+EXPOSE $BACKEND2_PORT
+
 COPY server2 /app/server
 
 WORKDIR /app/server
 
-EXPOSE $BACKEND2_PORT
+# Prefer not to run as root
+USER deno
+
+CMD [ "deno", "task", "start" ]
--- server2/main.ts
+++ server2/main.ts
@@ -3,7 +3,10 @@ import { serveStatic } from "hono/deno";
 
 const app = new Hono().basePath("/api2");
 
-app.use("/static/*", serveStatic({ root: "./static" }));
+app.use(
+  "/static/*",
+  serveStatic({ root: "./static", rewriteRequestPath: (path) => path.replace(/^\/api2\/static\//, "") }),
+);
 
 app.get("/", (c: Context) => {
   return c.text("Hello Hono!");
