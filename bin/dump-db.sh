cf5fb41d617264a3927716f46afdbd0ded0ed4c0
Deni <cube327@tuta.io>
2025-02-08 16:15:50 +0100

    Set up PostgreSQL

--- bin/dump-db.sh
+++ bin/dump-db.sh
@@ -11,32 +11,32 @@ elif [ ! -d "$1" ]; then
   exit 2
 else
   # Remove final / from the path (if it's present at all)
-  DUMP_PATH=$(echo "$1" | sed -E 's/\/$//')
+  dump_path=$(echo "$1" | sed -E 's/\/$//')
 fi
 
 if [ -n "$2" ]; then
-  DB_CONTAINER=$2
+  db_container=$2
 else
-  DB_CONTAINER=cc-mongo
+  db_container=cc-mongo
 fi
 
 # Check that the container with the DB is running
-if [ -z "$(sudo docker ps | grep "$DB_CONTAINER$")" ]; then
-  echo "Error: Container $DB_CONTAINER not found"
+if [ -z "$(sudo docker ps | grep "$db_container$")" ]; then
+  echo "Error: Container $db_container not found"
   exit 3
 fi
 
 # Create DB backup
-echo -e "Backing up \"cubingcontests\" database from the $DB_CONTAINER container...\n"
+echo -e "Backing up \"$DB_NAME\" database from the $db_container container...\n"
 source .env
-sudo docker exec $DB_CONTAINER \
-  sh -c "mongodump -u $MONGO_DEV_USERNAME -p $MONGO_DEV_PASSWORD --db cubingcontests && tar -cvz /dump/cubingcontests" > \
-  "$DUMP_PATH/backup_$(date "+%Y_%m_%d_%H_%M_%S").tar.gz" &&
+sudo docker exec $db_container \
+  sh -c "mongodump -u $DB_USERNAME -p $DB_PASSWORD --db $DB_NAME && tar -cvz /dump/$DB_NAME" > \
+  "$dump_path/backup_$(date "+%Y_%m_%d_%H_%M_%S").tar.gz" &&
 # Remove dump created by mongodump inside of the container in the previous command
-sudo docker exec $DB_CONTAINER sh -c "rm -rf /dump" &&
-echo -e "\nDatabase backed up to $DUMP_PATH"
+sudo docker exec $db_container sh -c "rm -rf /dump" &&
+echo -e "\nDatabase backed up to $dump_path"
 # Delete the oldest backup (get all files in, sort them alphabetically, skip the first line
 # that simply shows the path passed in with $1, and take the path to the first (oldest) backup)
-rm -f $(find "$DUMP_PATH" | sort | tail -n +2 | head -n 1) &&
+rm -f $(find "$dump_path" | sort | tail -n +2 | head -n 1) &&
 echo -e "\nOldest backup deleted\n\nCurrent backups:\n"
-ls "$DUMP_PATH"
+ls "$dump_path"
--- server/src/main.ts
+++ server/src/main.ts
@@ -3,9 +3,12 @@ import { AppModule } from "./app.module";
 import { MyLogger } from "./modules/my-logger/my-logger.service";
 
 async function bootstrap() {
+  if (!process.env.PORT) throw new Error("FRONTEND PORT NOT SET!");
+  if (!process.env.BACKEND_PORT) throw new Error("BACKEND PORT NOT SET!");
   if (!process.env.JWT_SECRET) throw new Error("JWT SECRET NOT SET!");
-  if (!process.env.MONGO_DEV_USERNAME) throw new Error("MONGO DB USERNAME NOT SET!");
-  if (!process.env.MONGO_DEV_PASSWORD) throw new Error("MONGO DB PASSWORD NOT SET!");
+  if (!process.env.DB_NAME) throw new Error("DB NAME NOT SET!");
+  if (!process.env.DB_USERNAME) throw new Error("DB USERNAME NOT SET!");
+  if (!process.env.DB_PASSWORD) throw new Error("DB PASSWORD NOT SET!");
   if (!process.env.BASE_URL) throw new Error("BASE URL NOT SET!");
   if (!process.env.MAIL_URL) throw new Error("MAIL URL NOT SET!");
   if (!process.env.EMAIL_USERNAME) throw new Error("EMAIL USERNAME NOT SET!");
@@ -18,9 +21,13 @@ async function bootstrap() {
   app.useLogger(logger); // use custom logger
 
   if (process.env.NODE_ENV === "production") {
-    corsOptions = { origin: [process.env.BASE_URL, "http://cc-client:3000"] };
+    corsOptions = {
+      origin: [process.env.BASE_URL, `http://cc-client:${process.env.PORT}`],
+    };
 
-    logger.log(`Setting CORS origin policy for ${corsOptions.origin.join(", ")}`);
+    logger.log(
+      `Setting CORS origin policy for ${corsOptions.origin.join(", ")}`,
+    );
   }
 
   app.enableCors(corsOptions);
