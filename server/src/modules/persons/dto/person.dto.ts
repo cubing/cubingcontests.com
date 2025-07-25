0d9fcc3ddfb50ce42f40a1adac6b7a41be32aeee
Deni <cube327@tuta.io>
2025-07-25 21:36:13 +0200

    Prohibit parentheses in names and localized names

--- server/src/modules/persons/dto/person.dto.ts
+++ server/src/modules/persons/dto/person.dto.ts
@@ -1,4 +1,10 @@
-import { IsIn, IsOptional, IsString, Matches, MinLength } from "class-validator";
+import {
+  IsIn,
+  IsOptional,
+  IsString,
+  Matches,
+  MinLength,
+} from "class-validator";
 import { Countries } from "~/helpers/Countries";
 import { getMinLengthOpts, invalidCountryOpts } from "~/src/helpers/validation";
 import { C } from "~/helpers/constants";
@@ -7,11 +13,17 @@ import { IPersonDto } from "~/helpers/types";
 export class PersonDto implements IPersonDto {
   @IsString()
   @MinLength(3, getMinLengthOpts("person name", 3))
+  @Matches(C.personNameRegex, {
+    message: "The name cannot contain parentheses",
+  })
   name: string;
 
   @IsOptional()
   @IsString()
   @MinLength(2, getMinLengthOpts("localized name", 2))
+  @Matches(C.personNameRegex, {
+    message: "The localized name cannot contain parentheses",
+  })
   localizedName?: string;
 
   @IsOptional()
