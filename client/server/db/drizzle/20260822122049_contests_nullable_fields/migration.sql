63321278061efb19ddfd5d759f5cc96ca911afed
Deni <cube327@tuta.io>
2026-08-24 11:56:53 +0100

    fix: contests_nullable_fields migration

--- client/server/db/drizzle/20260822122049_contests_nullable_fields/migration.sql
+++ client/server/db/drizzle/20260822122049_contests_nullable_fields/migration.sql
@@ -2,5 +2,5 @@ ALTER TABLE "record_ranks"."contests" ALTER COLUMN "venue" DROP NOT NULL;--> sta
 ALTER TABLE "record_ranks"."contests" ALTER COLUMN "address" DROP NOT NULL;--> statement-breakpoint
 ALTER TABLE "record_ranks"."contests" ALTER COLUMN "competitor_limit" DROP NOT NULL;
 -- CUSTOM ADDITION FOR RECORDRANKS!
-UPDATE "record_ranks"."contests" SET "venue" IS NULL WHERE "venue" = '';
-UPDATE "record_ranks"."contests" SET "address" IS NULL WHERE "address" = '';
+UPDATE "record_ranks"."contests" SET "venue" = NULL WHERE "venue" = '';
+UPDATE "record_ranks"."contests" SET "address" = NULL WHERE "address" = '';
