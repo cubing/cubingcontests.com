29009a327727cf53426907f4e661dbf0ea471a5d
Deni <cube327@tuta.io>
2026-09-01 08:36:19 +0100

    fix: minor UI tweak

--- client/app/[slug]/mod/api-keys/page.tsx
+++ client/app/[slug]/mod/api-keys/page.tsx
@@ -1,18 +1,28 @@
+import { SWRConfig } from "swr";
 import LoadingError from "~/app/components/UI/LoadingError.tsx";
 import ToastMessages from "~/app/components/UI/ToastMessages.tsx";
+import { SwrKey } from "~/helpers/swr-keys.ts";
 import type { ContestApiKeyMetadata } from "~/helpers/types.ts";
 import { auth } from "~/server/auth.ts";
 import { getModContestsSF } from "~/server/server-functions/contest-server-functions.ts";
-import { authorizeUser } from "~/server/server-only-functions/server-only-functions.ts";
+import { authorizeUser, getOrgDetails, getRegions } from "~/server/server-only-functions/server-only-functions.ts";
 import ManageApiKeysScreen from "./ApiKeysScreen.tsx";
 
-async function ApiKeysPage() {
+type Props = {
+  params: Promise<{
+    slug: string;
+  }>;
+};
+
+async function ApiKeysPage({ params }: Props) {
+  const { slug } = await params;
   const { httpHeaders } = await authorizeUser({
     useOrganization: true,
     orgPermissions: { modDashboard: ["view"], competitions: ["create", "update"], meetups: ["create", "update"] },
   });
 
-  const [contestsRes, apiKeys] = await Promise.all([
+  const [organization, contestsRes, apiKeys] = await Promise.all([
+    getOrgDetails({ slug }),
     getModContestsSF({}),
     auth.api
       .listApiKeys({
@@ -37,17 +47,21 @@ async function ApiKeysPage() {
 
   if (!contestsRes.data) return <LoadingError loadingEntity="api keys" />;
 
+  const regionsPromise = getRegions(organization.id);
+
   return (
-    <section>
-      <h2 className="mb-4 text-center">API Keys</h2>
+    <SWRConfig value={{ fallback: { [SwrKey.Regions]: regionsPromise } }}>
+      <section>
+        <h2 className="mb-4 text-center">API Keys</h2>
 
-      <ToastMessages className="mx-2" />
+        <ToastMessages className="mx-2" />
 
-      <ManageApiKeysScreen
-        contests={contestsRes.data!.contests.filter((c) => c.state !== "created")}
-        apiKeys={apiKeys}
-      />
-    </section>
+        <ManageApiKeysScreen
+          contests={contestsRes.data!.contests.filter((c) => c.state !== "created")}
+          apiKeys={apiKeys}
+        />
+      </section>
+    </SWRConfig>
   );
 }
 
